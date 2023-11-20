db572f189a22490717a2127c691b1f16d5d374b2
Fish <sm29729443@gmail.com>
2023-11-20 11:48:26 +0800

    initial spring boot project jpa

---
+++ src/main/java/com/example/springbootmalljpa/SpringbootMallJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmalljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmalljpa/SpringbootMallJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmalljpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
