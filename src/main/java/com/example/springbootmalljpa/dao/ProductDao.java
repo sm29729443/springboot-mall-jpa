7df3f6b829c5f2b40bb83cc814fe1757dfd91e01
Fish <sm29729443@gmail.com>
2023-11-21 12:53:02 +0800

    修訂1:查詢商品底層修正，連動到修改商品 修訂2:新增exception處理查詢商品時id為null情況

--- src/main/java/com/example/springbootmalljpa/dao/ProductDao.java
+++ src/main/java/com/example/springbootmalljpa/dao/ProductDao.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+
 /**
  * ClassName: ProductDao
  * Package: com.example.springbootmalljpa.dao
@@ -12,4 +14,7 @@ import org.springframework.data.repository.query.Param;
 public interface ProductDao extends JpaRepository<ProductEntity, Integer> {
     @Query(value = "SELECT * FROM product WHERE product_id = :product_id",nativeQuery = true)
     public ProductEntity getProductById(@Param("product_id") Integer productId);
+
+    @Query(value = "SELECT created_date FROM product WHERE product_id = :productId",nativeQuery =true)
+    public Date getCreatedDateById(Integer productId);
 }
